cbacd8539aa7dbd7af6c98d71f83e57c2db5e702
Sergey B <serget@zipoapps.com>
2022-04-04 21:21:02 +0300

    Styles screen layout

--- app/src/main/java/it/coffee/smartcoffee/presentation/style/StyleViewModel.kt
+++ app/src/main/java/it/coffee/smartcoffee/presentation/style/StyleViewModel.kt
@@ -1,28 +1,50 @@
 package it.coffee.smartcoffee.presentation.style
 
+import androidx.annotation.DrawableRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import it.coffee.smartcoffee.domain.*
+import it.coffee.smartcoffee.R
+import it.coffee.smartcoffee.domain.CoffeeRepository
+import it.coffee.smartcoffee.domain.Failure
+import it.coffee.smartcoffee.domain.Success
 import it.coffee.smartcoffee.domain.model.CoffeeType
 import kotlinx.coroutines.launch
 
 class StyleViewModel(private val machine_id: String, private val repository: CoffeeRepository) : ViewModel() {
 
-    private val _styles = MutableLiveData<List<CoffeeType>>()
-    val styles : LiveData<List<CoffeeType>> = _styles
+    private val _items = MutableLiveData<List<StyleListItem>>()
+    val items : LiveData<List<StyleListItem>> = _items
+
+    private var styles: List<CoffeeType>? = null
 
     init {
         viewModelScope.launch {
-
-            val result = repository.getTypes(machine_id)
-
-            when (result) {
-                is Success -> _styles.value = result.value
+            when (val result = repository.getTypes(machine_id)) {
+                is Success -> {
+                    styles = result.value
+                    _items.value = styles?.map {
+                        StyleListItem(it.id, getIcon(it.name), it.name)
+                    }
+                }
                 is Failure -> error(result.exception)
             }
+        }
+    }
 
+    fun getCoffeeType(styleId: String): CoffeeType {
+        return styles?.find { it.id == styleId } ?: error("Style not found")
+    }
+
+    // TODO: Bad idea to get icon based on the coffee name but I am not sure if type ids are stable
+    private fun getIcon(name: String): Int {
+        return when (name.lowercase()) {
+            "cappuccino" -> R.drawable.ic_cappuccino
+            "espresso" -> R.drawable.ic_espresso
+            else -> R.drawable.ic_coffee_medium
         }
     }
 }
+
+data class StyleListItem(val id: String, @DrawableRes val icon: Int, val name: String)
---
+++ app/src/main/java/it/coffee/smartcoffee/presentation/style/StylesAdapter.kt
@@ -0,0 +1,31 @@
+package it.coffee.smartcoffee.presentation.style
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import it.coffee.smartcoffee.R
+
+class StylesAdapter(private val items: List<StyleListItem>, private val listener: (style: StyleListItem) -> Unit) : RecyclerView.Adapter<StylesAdapter.StyleViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StyleViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return StyleViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StyleViewHolder, position: Int) {
+        holder.textView.text = items[position].name
+        holder.textView.setCompoundDrawablesWithIntrinsicBounds(items[position].icon, 0, 0, 0)
+        holder.textView.setOnClickListener {
+            listener(items[position])
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+    class StyleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView as TextView
+    }
+
+}
